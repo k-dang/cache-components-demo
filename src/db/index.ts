07eecfd665c27d522c78a8ab6fab9212fc875eca
Kevin Dang <dangkevin12@gmail.com>
2025-10-24 19:35:25 -0400

    fix types

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { pokemonTable } from "./schema";
 import { sql } from "drizzle-orm";
 
-const db = drizzle(process.env.DATABASE_URL);
+const db = drizzle(process.env.DATABASE_URL!);
 
 export async function getRandomPokemon(limit = 24) {
   const pokemon = await db
